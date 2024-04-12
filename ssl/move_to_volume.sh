d73a8f4979f44eee4fd66e8b99e5ab66964d7f6c
reudekx <reudekx@gmail.com>
2024-04-12 16:17:25 +0900

    feat: Modify to use nfs volume

--- ssl/move_to_volume.sh
+++ ssl/move_to_volume.sh
@@ -3,12 +3,19 @@
 # Get the absolute path of the current directory
 current_dir=$(pwd)
 
-# Create certbot_conf volume (if it doesn't exist) and copy contents from ./data/certbot/conf
-docker volume create certbot_conf
-docker run --rm -v "$current_dir/../data/certbot/conf":/src -v certbot_conf:/dest busybox sh -c "rm -rf /dest/* && cp -r /src/. /dest/"
+# NFS server IP address
+nfs_server="climateprediction.xyz"
 
-# Create certbot_www volume (if it doesn't exist) and copy contents from ./data/certbot/www
-docker volume create certbot_www
-docker run --rm -v "$current_dir/../data/certbot/www":/src -v certbot_www:/dest busybox sh -c "rm -rf /dest/* && cp -r /src/. /dest/"
+# NFS server path for certbot_conf volume
+nfs_path_conf="$current_dir/../data/certbot/conf"
 
-echo "Volumes created and data copied successfully (overwritten if already existed)!"
+# NFS server path for certbot_www volume
+nfs_path_www="$current_dir/../data/certbot/www"
+
+# Create certbot_conf volume (if it doesn't exist) with NFS options
+docker volume create --driver local --opt type=nfs --opt o=addr=$nfs_server,rw --opt device=:$nfs_path_conf certbot_conf
+
+# Create certbot_www volume (if it doesn't exist) with NFS options
+docker volume create --driver local --opt type=nfs --opt o=addr=$nfs_server,rw --opt device=:$nfs_path_www certbot_www
+
+echo "NFS volumes created successfully (overwritten if already existed)!"
